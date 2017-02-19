c8b2e0c14d8528603fe3d3ac0642cdd49a3bce1c
Brenton Holswich <brenton.holswich@gmail.com>
2017-02-19 02:06:31 +0000

    brought in the range array and successfully count between the two numbers

--- loopy-lighthouse.js
+++ loopy-lighthouse.js
@@ -1,12 +1,29 @@
-// count from 100 to 200
-for(var i = 100; i < 201; i++) {
-  if (i % 3 === 0 && i % 4 === 0) {
-    console.log("LoopyLighthouse");
-  }else if (i % 3 === 0) {
-    console.log("Loopy");
-  }else if (i % 4 === 0) {
-    console.log("Lighthouse");
-  }else {
-    console.log(i);
+// // count from 100 to 200
+// for(var i = 100; i < 201; i++) {
+//   if (i % 3 === 0 && i % 4 === 0) {
+//     console.log("LoopyLighthouse");
+//   }else if (i % 3 === 0) {
+//     console.log("Loopy");
+//   }else if (i % 4 === 0) {
+//     console.log("Lighthouse");
+//   }else {
+//     console.log(i);
+//   }
+// }
+
+var loopyLighthouse = function(range, multiples, words) {
+  // count between our range array numbers
+  for(var i = range[0]; i < range[1]+1; i++) {
+    if (i % 3 === 0 && i % 4 === 0) {
+      console.log("LoopyLighthouse");
+    }else if (i % 3 === 0) {
+      console.log("Loopy");
+    }else if (i % 4 === 0) {
+      console.log("Lighthouse");
+    }else {
+      console.log(i);
+    }
   }
 }
+
+loopyLighthouse([1,50]);
