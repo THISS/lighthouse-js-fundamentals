bb691fca4ece20597461c13541655d4ab549982a
Brenton Holswich <brenton.holswich@gmail.com>
2017-02-19 02:20:30 +0000

    added space in the for loop test section for the addition operator as the linter required

--- loopy-lighthouse.js
+++ loopy-lighthouse.js
@@ -13,7 +13,7 @@
 
 var loopyLighthouse = function(range, multiples, words) {
   // count between our range array numbers
-  for(var i = range[0]; i < range[1]+1; i++) {
+  for(var i = range[0]; i < range[1] + 1; i++) {
     // test against both multiples
     if (i % multiples[0] === 0 && i % multiples[1] === 0) {
       // concatenate the words array
